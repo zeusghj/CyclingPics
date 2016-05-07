02a50f4d8d20d0dc23b663af1beb2426330b7380
HongJun <zeusghj@163.com>
2016-05-07 16:28:13 +0800

    a new project of ios无限图片轮播器

---
+++ CyclingPics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyclingPics
+//
+//  Created by 郭洪军 on 5/7/16.
+//  Copyright © 2016 郭洪军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
